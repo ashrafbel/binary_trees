5f418cf2321849548d53d85cf525fbbf3da402df
ashrafbel <ashrafmus2@gmail.com>
2024-06-05 22:06:40 -0700

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,7 +11,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	else if (tree && func)
 	{
 		binary_tree_postorder(tree->left, func);
-		binary_tree_postorder(tree->right, func)
+		binary_tree_postorder(tree->right, func);
 		func(tree->n);
 	}
 }
