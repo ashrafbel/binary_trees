dfa3f52d60baf53d3c123069d03de86812a68f2c
ashrafbel <ashrafmus2@gmail.com>
2024-06-06 19:05:03 -0700

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,7 +6,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t HeightOfLeft = 0, HeightOfright = 0;
+	size_t HeightOfLeft = 1, HeightOfright = 1;
 
 	if (tree == NULL)
 		return (0);
