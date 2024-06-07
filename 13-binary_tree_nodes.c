c2f0acce24904cda69c22d3031f3bbc81bfff79b
ashrafbel <ashrafmus2@gmail.com>
2024-06-06 18:32:42 -0700

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,9 +12,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
-		C = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+	{
+		C = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 		return (C);
+	}
 	else
 		return (0);
+
 }
 
