37f94fe53c3e4f04824ceeff51fb984c5ecd4c6f
ashrafbel <ashrafmus2@gmail.com>
2024-06-06 19:51:43 -0700

    16-

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,7 +6,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t HeightOfLeft = 1, HeightOfright = 1;
+	size_t HeightOfLeft = 0, HeightOfright = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -40,4 +40,22 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 			return (0);
 	}
 	return (0);
+}
+/**
+ * binary_tree_is_full - checks whether a binary tree is full or not
+ * @tree: Ptr to the root node of the tree to check.
+ * Return: 1 if the tree is full, 0 otherwise or if tree is NULL.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int full_;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		full_ = 1;
+	full_ = binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+	return (full_);
 }
